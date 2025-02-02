ff3020359519791a1dc5ef43fc51784287edd267
solebellsBEACH <lucassxxavier@gmail.com>
2025-02-02 15:04:39 -0300

    feat: adding core configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,39 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    'node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary-color)",
+        secondary: "var(--secondary-color)",
+        accent: "var(--accent-color)",
+        backgroundColor: "var(--background-color)",
+        text: "var(--text-color)",
+        buttonHover: "var(--button-hover-color)",
+        border: "var(--border-color)",
+        success: "var(--success-color)",
+        warning: "var(--warning-color)",
+        error: "var(--error-color)",
+        info: "var(--info-color)",
+        lightGray: "var(--light-gray)",
+        darkGray: "var(--dark-gray)",
+        muted: "var(--muted-color)",
+        highlight: "var(--highlight-color)",
+        overlay: "var(--overlay-color)",
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [
+    require('preline/plugin'),
+  ],
+};
+
+export default config;
